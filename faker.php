57ef8480fa67b2f828fce5eb6d72d2debdb5f8f8
Puleeno Nguyen <puleeno@gmail.com>
2019-10-18 00:26:33 +0700

    Done faker

--- faker.php
+++ faker.php
@@ -1,1 +1,11 @@
 <?php
+
+add_action('plugins_loaded', 'master_slidear_license_faker');
+function master_slidear_license_faker()
+{
+    $license = new Puleeno\MasterSlider\License();
+    $domain = defined('MASTER_SLIDER_ACTIVE_DOMAIN') ? constant('MASTER_SLIDER_ACTIVE_DOMAIN') : '';
+
+    $license->setActiveDomain(apply_filters('master_slider_active_domain', $domain));
+    $license->fake();
+}
--- src/License.php
+++ src/License.php
@@ -1,5 +1,35 @@
 <?php
 namespace Puleeno\MasterSlider;
 
-class License {
+class License
+{
+    protected $domain;
+
+    public function __construct($domain = null)
+    {
+        if (!is_null($domain)) {
+            $this->domain = $domain;
+        }
+    }
+    public function setActiveDomain($domain)
+    {
+        $this->domain = $domain;
+    }
+
+    public function fake_request_info($r, $url)
+    {
+        if (false !== strpos($url, 'support.averta.net')) {
+            $r['user-agent'] = str_replace(get_site_url(), $this->domain, $r['user-agent']);
+            $r['body']['url'] = $this->domain;
+        }
+        return $r;
+    }
+
+    public function fake()
+    {
+        if (!wp_http_validate_url($this->domain)) {
+            return;
+        }
+        add_action('http_request_args', array($this, 'fake_request_info'), 10, 2);
+    }
 }
